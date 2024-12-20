df999f250620e4b36583713b96f2960d3fd32623
varuntree <117532982+varuntree@users.noreply.github.com>
2024-12-20 20:30:49 +0000

    Restored to '768112032469f1af9250fc1f325627fa4368d7b9'
    Replit-Restored-To:768112032469f1af9250fc1f325627fa4368d7b9

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-
 import Image from "next/image";
-import Navbar from "./components/Navbar";
-import Header from "./pages/Header";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <Header />
+      helo
     </div>
   );
 }
