8d2d803ccddbca8cc785f37aa89a8c64c75a1d28
varuntree <117532982+varuntree@users.noreply.github.com>
2024-12-22 04:46:21 +0000

    Restored to '609c4c5b9023b3805eda82aafd583da58651d46c'
    Replit-Restored-To:609c4c5b9023b3805eda82aafd583da58651d46c

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-
-"use client";
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 
-"use client";
-
 import Image from "next/image";
 import Header from "./pages/Header";
 
