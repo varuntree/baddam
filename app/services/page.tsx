a0cf03a67e0f0d5614dc3273e810f1c107f86f4e
varuntree <117532982+varuntree@users.noreply.github.com>
2024-12-25 05:52:40 +0000

    Automatic commit before Assistant edits

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
