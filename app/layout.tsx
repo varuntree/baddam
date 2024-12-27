b8b524b261c27b912bf60103ee01702b3c7d078c
varuntree <117532982+varuntree@users.noreply.github.com>
2024-12-27 01:18:06 +0000

    Automatic commit before Assistant edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MouseFollower from "./components/MouseFollower";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MouseFollower />
-        {children}
+        
+          {children}
       </body>
     </html>
   );
